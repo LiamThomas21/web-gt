6c5be7d837e7e4c31b7879248010e51bc6e576ee
Liam Thomas <123399807+LiamThomas21@users.noreply.github.com>
2024-02-11 21:41:41 +0200

    Add files via upload

---
+++ src/components/Navbar/navData.js
@@ -0,0 +1,27 @@
+export const navLinks = [
+  {
+    label: 'About',
+    pathname: '/#project-1',
+  },
+  {
+    label: 'Mission',
+    pathname: '/#details',
+  },
+  {
+    label: 'Contact',
+    pathname: '/contact',
+  },
+];
+
+export const socialLinks = [
+  {
+    label: 'Twitter',
+    url: 'https://twitter.com/hamishMW',
+    icon: 'twitter',
+  },
+  {
+    label: 'Figma',
+    url: 'https://www.figma.com/@Hamish',
+    icon: 'figma',
+  },
+];
